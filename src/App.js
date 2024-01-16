9ae2460024de8d8357e381b70bca000dcd030351
Amandaaaz <amandazfernandes@hotmail.com>
2024-01-16 10:45:05 -0300

    16/01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import { useState } from "react";
-
-import jpIMG from "./assets/jp.svg";
+import jpIMG from "./assets/logo.png";
 import "./styles.css";
 
 function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isCadastro, setCadastro] = useState(false);
+  const [nome, setNome] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
+  const [cadastroSucesso, setCadastroSucesso] = useState(false);
+
+  const handleToggleCadastro = () => {
+    setCadastro(!isCadastro);
+    setCadastroSucesso(false); 
+  };
+
+  const handleCadastro = (e) => {
+    e.preventDefault();
+
+    setCadastroSucesso(true);
+    console.log("Cadastro realizado com sucesso!");
+  };
 
   return (
     <div className="container">
       <div className="container-login">
         <div className="wrap-login">
           <form className="login-form">
-            <span className="login-form-title"> Bem vindo </span>
-
+            <span className="login-form-title">{isCadastro ? "Cadastro" : "Bem vindo"}</span>
             <span className="login-form-title">
               <img src={jpIMG} alt="Jovem Programador" />
             </span>
-
+  
+            {isCadastro && (
+              <div className="wrap-input">
+                <input
+                  className={nome !== "" ? "has-val input" : "input"}
+                  type="text"
+                  value={nome}
+                  onChange={(e) => setNome(e.target.value)}
+                />
+                <span className="focus-input" data-placeholder="Nome"></span>
+              </div>
+            )}
+  
             <div className="wrap-input">
               <input
                 className={email !== "" ? "has-val input" : "input"}
@@ -27,7 +53,7 @@ function App() {
               />
               <span className="focus-input" data-placeholder="Email"></span>
             </div>
-
+  
             <div className="wrap-input">
               <input
                 className={password !== "" ? "has-val input" : "input"}
@@ -35,24 +61,51 @@ function App() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
               />
-              <span className="focus-input" data-placeholder="Password"></span>
+              <span className="focus-input" data-placeholder="Senha"></span>
             </div>
-
+  
+            {isCadastro && (
+              <div className="wrap-input">
+                <input
+                  className={confirmarSenha !== "" ? "has-val input" : "input"}
+                  type="password"
+                  value={confirmarSenha}
+                  onChange={(e) => setConfirmarSenha(e.target.value)}
+                />
+                <span className="focus-input" data-placeholder="Confirmar Senha"></span>
+              </div>
+            )}
+  
             <div className="container-login-form-btn">
-              <button className="login-form-btn">Login</button>
+              {!cadastroSucesso ? (
+                <button className="login-form-btn" onClick={isCadastro ? handleCadastro : () => {}}>
+                  {isCadastro ? "Finalizar" : "Login"}
+                </button>
+              ) : null}
             </div>
-
+  
             <div className="text-center">
-              <span className="txt1">Não possui conta? </span>
-              <a className="txt2" href="#">
-                Criar conta
-              </a>
+              {cadastroSucesso ? (
+                <p className="success-message">
+                  Cadastro efetuado com sucesso! Verifique sua caixa de email para finalizar seu cadastro.
+                  <a className="txt2 highlighted" href="#" onClick={handleToggleCadastro}>
+                    Voltar para o Login
+                  </a>
+                </p>
+              ) : (
+                <div>
+                  <span className="txt1">{isCadastro ? "Já possui uma conta?" : "Não possui conta?"}</span>
+                  <a className="txt2" href="#" onClick={handleToggleCadastro}>
+                    {isCadastro ? "Voltar para o Login" : "Criar conta"}
+                  </a>
+                </div>
+              )}
             </div>
           </form>
         </div>
       </div>
     </div>
   );
-}
+}  
 
 export default App;
